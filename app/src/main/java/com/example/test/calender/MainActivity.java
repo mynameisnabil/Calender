23494716d781ea935ecb8650f78efe134173c99f
mynameisnabil <43395606+mynameisnabil@users.noreply.github.com>
2018-11-06 21:36:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/test/calender/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.test.calender;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.CalendarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "CalendarActivity";
+    private CalendarView mCalendarView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mCalendarView = (CalendarView) findViewById(R.id.calendarView);
+        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView CalendarView, int year, int month, int dayOfMonth) {
+                String date = year + "/" + month + "/" + dayOfMonth;
+                Log.d(TAG, "onSelectedDayChange: yyyy/mm/dd:" + date);
+
+
+            }
+        });
+    }
+}
